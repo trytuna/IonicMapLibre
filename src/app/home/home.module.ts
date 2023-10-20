a804a173f9912aa4f50ad7e9b4f16b0f883fad3f
Timo Schrappe <timo.schrappe@tu-dortmund.de>
2023-10-20 12:57:14 +0200

    Add maplibre component with resize hack

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { NgxMapLibreGLModule } from '@maplibre/ngx-maplibre-gl';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgxMapLibreGLModule
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MapComponent } from '@maplibre/ngx-maplibre-gl';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(MapComponent) private map: MapComponent | null = null;
 
   constructor() {}
+
+  ngAfterViewInit() {
+    /**
+     * This hack is needed to get the map take up the full height.
+     * Unfortunately this does not work in Safari.
+     * 
+     * Source: https://github.com/maplibre/ngx-maplibre-gl/issues/92
+     */
+    setTimeout(() => this.map!.mapInstance.resize(), 0);
+  }
 
 }
